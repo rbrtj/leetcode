08751c682da3a65de69ab37d3f86db9112c71272
rbrtj <robertjaszczurek@icloud.com>
2024-05-05 21:34:08 +0200

    binary search

---
+++ binary-search/binary-search.ts
@@ -0,0 +1,20 @@
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;
+      continue;
+    }
+    if (nums[mid] > target) {
+      right = mid - 1;
+      continue;
+    }
+  }
+  return -1;
+}
