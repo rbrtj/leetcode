c839be4917f771c8032d20bba78307bd336d8112
rbrtj <robertjaszczurek@icloud.com>
2024-05-19 22:15:00 +0200

    reverse string

---
+++ two-pointers/reverse-string/reverse-string.js
@@ -0,0 +1,12 @@
+function reverseString(s) {
+  let left = 0;
+  let right = s.length - 1;
+  let temp = "";
+  while (left < right) {
+    temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+  }
+}
